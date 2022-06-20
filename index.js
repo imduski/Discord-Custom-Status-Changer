f3d4e00c6dca881ff1373f162e35a820f370b910
imduski <98903401+imduski@users.noreply.github.com>
2022-06-20 23:16:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const keepAlive = require("./server")
 const request = require('request');
 const config = require('./config.json');
-const mySecret = process.env['token']
 const statuses = config.statuses;
 var status_index = 0;
 
@@ -18,7 +17,7 @@ setInterval(() => {
         json: true,
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36',
-            'Authorization': process.env['token'],
+            'Authorization': client.login(token),
             'Content-Type': 'application/json'
         },
         body: {
@@ -30,3 +29,6 @@ setInterval(() => {
     })
     console.log('Status updated');
 }, config.time)
+
+
+client.login("")
